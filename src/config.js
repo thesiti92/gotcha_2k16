91f0e6db84efc54f4b09a2cb2aac1672d0f5914e
Alexander Iansiti <aiansiti@outlook.com>
2016-08-12 16:31:43 -0400

    Auth works. my head hurts.
     someone give me head pls. can be male or female.

---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+var config = {
+    apiKey: "AIzaSyC7KvV36XoQff6eVp16Kem9F-FPWxN_BvM",
+    authDomain: "gotcha-2k16.firebaseapp.com",
+    databaseURL: "https://gotcha-2k16.firebaseio.com",
+    storageBucket: "gotcha-2k16.appspot.com"
+};
+module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
 
-var Todo = require('./todo');
+var firebase = require("firebase/app");
+require("firebase/auth");
+firebase.initializeApp(require("./config"));
+var provider = new firebase.auth.GoogleAuthProvider();
 
-ReactDOM.render(
-  <Todo />,
-  document.getElementById('app')
-);
+var signIn = function() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var newUser = result.user;
+        console.log(newUser.email);
+        if (newUser.email.includes("milton.edu")) {
+            loggedIn(newUser);
+            return true;
+        }
+
+        newUser.delete();
+        signIn();
+        return false;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log([errorCode, errorMessage, email, credential]);
+        signIn();
+    });
+};
+
+var user = firebase.auth().currentUser;
+
+if (user) {
+    console.log("yay");
+    loggedIn(user);
+} else {
+    console.log(user);
+    ReactDOM.render(
+        <button onClick={signIn}>Sign In</button>, document.getElementById('app'));
+}
+
+function loggedIn(user) {
+    var data = <div>Hello {user.displayName}</div>;
+    ReactDOM.render(data, document.getElementById('app'));
+}
