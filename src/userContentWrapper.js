9a1703861ce85559b0aba20c459fd42cc7b13f63
Alexander Iansiti <aiansiti@outlook.com>
2016-08-15 11:17:55 -0400

    now the client knows when its gotten got

--- src/userContentWrapper.js
+++ src/userContentWrapper.js
@@ -1,23 +1,34 @@
 var React = require('react');
 var firebase = require("firebase/app");
 require("firebase/auth");
+require("firebase/database");
 var reactfire = require('reactfire');
 
 var UserContent = React.createClass({
     mixins: [reactfire],
     getInitialState: function() {
-        return {text: 'text'};
+        var user = firebase.auth().currentUser;
+        return {user: {}, name: user.displayName};
     },
-    gotcha: function(e) {
-      e.preventDefault();
-        this.setState({
-            text: "gotcha"
+    componentWillMount: function() {
+        var ref = firebase.database().ref(this.state.name);
+        this.bindAsObject(ref, "user");
+        this.firebaseRefs.user.on('child_changed', function(snapshot) {
+            if (snapshot.val() === 0) {
+                alert("you got got bruh");
+            }
         });
     },
+    gotcha: function(e) {
+        e.preventDefault();
+        console.log(this.state.user.email);
+        this.firebaseRefs.user.update({alive: 0});
+    },
+
     render: function() {
         return (
             <button onClick={this.gotcha}>
-                {this.state.text}
+                {this.state.user.email}
             </button>
         );
     }
